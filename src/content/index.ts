3e56d466a3b2095a3d1871cf58ad78c8b1b6aa83
bartoshko <lenart.consulting@gmail.com>
2019-04-13 22:51:34 +0200

    timeout to be sure page is fully loaded, scrapper requirement

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,8 +2,10 @@ import './style.scss';
 import {Scrapper} from './scrapper';
 
 window.onload = function (): void {
-	const text = new Scrapper().getPageText();
-	if (text) {
-		chrome.runtime.sendMessage({message: text});
-	}
+	setTimeout(() => {
+		const text = new Scrapper().getPageText();
+		if (text) {
+			chrome.runtime.sendMessage({message: text});
+		}
+	}, 5000);
 };
