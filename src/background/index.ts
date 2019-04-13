315b51981e5b5644d0ddf53cfc0efe691f7c6f10
Maciej Radzikowski <mradzikowski@blastlab.co>
2019-04-13 22:24:28 +0200

    Sending audio to background page as base64

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,3 +2,7 @@ import './hot-reload.ts';
 import {AudioRecordManager} from './audio-record-manager';
 
 AudioRecordManager.getInstance().init();
+
+chrome.runtime.onMessage.addListener(request => {
+	console.log('Received message', request);
+});
